ef7b63ad7a8e961a794939cfb61edfef1571edcc
Liubov-Knyazevskaya <liubanya09@gmail.com>
2022-10-29 21:30:23 +0300

    checked r esult, max number min number found

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, 
+// какое число большее, а какое меньшее.
+
+//      num1  a = 5;  num2 b = 7 -> max = 7
+//      a = 2 b = 10 -> max = 10
+//      a = -9 b = -3 -> max = -3
+// num1  a = 5;  num2 b = 7 -> max = 7
+
+System.Console.WriteLine("Enter two numbers");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (num2 > num1)
+{
+    System.Console.WriteLine($"максимальное число {num2}, минимальное число {num1} ");
+}
+if (num1 > num2)
+{  
+    System.Console.WriteLine($"максимальное число {num1}, минимальное число {num2} ");;
+}
