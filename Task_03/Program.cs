cd3edae4e277f73da8cbe963d7eed8feb980bee5
Liubov-Knyazevskaya <liubanya09@gmail.com>
2022-10-30 01:28:23 +0300

    add task%2=0

--- Task_03/Program.cs
+++ Task_03/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Enter number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = (num%2);
+if (result == 0)  // переменная , свойство или индексатор!
+{
+    System.Console.WriteLine($"да");            
+}
+else 
+{
+    System.Console.WriteLine($"нет");
+}
