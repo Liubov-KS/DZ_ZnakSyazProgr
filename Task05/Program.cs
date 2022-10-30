49b947903888387faa82033e1a274310c45ec85e
Liubov-Knyazevskaya <liubanya09@gmail.com>
2022-10-30 04:03:02 +0300

    add Program file

--- Task05/Program.cs
+++ Task05/Program.cs
@@ -7,9 +7,9 @@ int daynum = Convert.ToInt32(Console.ReadLine());
 
 if(daynum == 6 || daynum == 7)
   {
-  Console.WriteLine($"да -> это выходной");
+  Console.WriteLine("да -> это выходной");
   }
   else if (daynum < 1 || daynum > 7) {
-    Console.WriteLine("цифра менье 1 и больше 7, это не день недели, ввдите цифру от 1 до 7");
+    Console.WriteLine("цифра меньше 1 и больше 7, введите цифру от 1 до 7");
   }
   else {Console.WriteLine("нет -> это не выходной");}
