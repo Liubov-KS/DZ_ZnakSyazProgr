a84902ebb812098f7f438ac67b65d0cce94bc277
Liubov-Knyazevskaya <liubanya09@gmail.com>
2022-10-30 19:29:42 +0300

    Add Var % and / to search SecondNum from TreeDigitnumber

--- Task07/Program.cs
+++ Task07/Program.cs
@@ -3,8 +3,18 @@
 //782 -> 8
 //918 -> 1
 
-System.Console.WriteLine("Введите трехзначное число");
-int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+// вар стринг ++++ индекс
 
-string stringNumber = Convert.ToString(threeDigitNumber);
-System.Console.WriteLine("вторая цифра числа, -> "+stringNumber[1]);
+     System.Console.WriteLine("Введите трехзначное число");
+     int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+     string stringNumber = Convert.ToString(threeDigitNumber);
+     System.Console.WriteLine("вторая цифра числа, -> "+stringNumber[1]);
+
+//  ++++++++ вариант через % и /  ++++++++++
+System.Console.WriteLine("Введите трехзначное число");
+int threeDigitNumber = Convert.ToInt32(Console.ReadLine()); 
+
+int result = threeDigitNumber%100 / 10;
+
+System.Console.WriteLine($"показываем вторую цифру числа: {result}");
