49d2edd8b367ad73bf1f0f36ce2568c1aa885dc8
Liubov-Knyazevskaya <liubanya09@gmail.com>
2022-11-05 22:17:41 +0300

    add bool  isPalindrome

--- Task8_/Program.cs
+++ Task8_/Program.cs
@@ -1,10 +1,11 @@
 ﻿/* Задача 19
 Напишите программу, которая принимает на вход пятизначное число и проверяет, 
 является ли оно палиндромом.
-14212 -> нет    12821 ->    23432 -> да
+14212 -> нет    
+12821 ->да    
+23432 -> да
 */
-
-Console.Write("Введите 5-ти значное число: ");
+ Console.Write("Введите 5-ти значное число: ");
 string number = Console.ReadLine();
 
 void CheckingNumber(string number)
@@ -21,3 +22,28 @@ if (number!.Length == 5)
   CheckingNumber(number);
 }
 else Console.WriteLine($"Введите правильное число");
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Palindrome bool
+
+class Program
+{
+  static void Main(string[] args)
+{
+  int numPalindrome = SetNumber("Palindrome");
+
+  bool isPalindrome = isPalindromeInt(numPalindrome);
+
+  string str = isPalindrome ? "является палиндромом" : "не является палндромом";
+
+  if (isPalindrome)
+  {
+    string str = "является палиндромом";
+  }
+  else
+  {
+    string str = "не является палиндромом";
+  }
+  System.Console.WriteLine($"число {numPalindrome} {str}");
+}
+}
